275e22b09b18d652906a21916a099a09bc059a3a
Ishvarloiva <Ishvarloiva@gmail.com>
2014-09-08 19:06:57 +0300

    Snake

---
+++ src/core/Snake.java
@@ -0,0 +1,14 @@
+package core;
+
+import screen.Snake_window;
+import variables.Variables;
+
+public class Snake {
+
+	public static void main(String[] args)
+	{
+		Variables vars = new Variables();
+		@SuppressWarnings("unused")
+		Snake_window w = new Snake_window(vars);
+	}
+}
